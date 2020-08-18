3054ea7c979760a63ede673141816a361e59822f
Jon Selzer <jselzer626@gmail.com>
2020-08-18 15:01:16 -0400

    added player db models

--- FFLO_Server_Django/urls.py
+++ FFLO_Server_Django/urls.py
@@ -14,10 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
-    path("", include("players.urls"),
-    path("users/", include("users.urls")),
     path('admin/', admin.site.urls),
 ]
--- players/models.py
+++ players/models.py
@@ -3,8 +3,8 @@ from django.conf import settings
 
 # Create your models here.
 class Roster(models.Model):
-    name=models.Charfield(max_length=30)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    name=models.CharField(max_length=30)
+    user=models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 
 class Player(models.Model):
@@ -15,7 +15,7 @@ class Player(models.Model):
     team=models.CharField(max_length=3)
     roster=models.ManyToManyField(Roster, blank=True)
     ranking=models.IntegerField(default=999)
-    profileImg=models.Charfield(max_length=100)
+    profileImg=models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.displayName} {self.position} {self.team}"
