e26b67d38fef0900c70fb27033459235519e9ebd
Jon Selzer <jselzer626@gmail.com>
2020-08-17 23:52:43 -0400

    dupes from player list

--- scraper.py
+++ scraper.py
@@ -10,13 +10,21 @@ fb_base_url = 'https://www.nfl.com/players/'
 data = requests.get('https://www.fantasyfootballnerd.com/service/players/json/8qb63ck2ibj4/')
 dataJson = data.json()
 players = dataJson["Players"]
-testPlayer = players[0]['displayName']
-testPlayerSearchable = testPlayer.replace(" ", "-")
-print(testPlayer)
+duplicate_names = []
+for i in range(1, len(players)):
+    if players[i-1]['displayName'] == players[i]['displayName']:
+        duplicate_names.append(players[i-1])
+        duplicate_names.append(players[i])
 
-driver = webdriver.Chrome(executable_path=r'C:/webdrivers/chromedriver.exe')
+print(duplicate_names)
+
+'''testPlayer = players[0]['displayName']
+testPlayerSearchable = testPlayer.replace(" ", "-")'''
+
+
+'''driver = webdriver.Chrome(executable_path=r'C:/webdrivers/chromedriver.exe')
 driver.get(f"{fb_base_url}/{testPlayerSearchable}")
 content = driver.page_source
 soup=BeautifulSoup(content)
 tag = soup.find('img', attrs={'alt': testPlayer, 'class': 'img-responsive'})
-print(tag.attrs['src'])
+print(tag.attrs['src'])'''
