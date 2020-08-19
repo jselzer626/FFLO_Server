4b2ef5a2708a5082f1eb05093305b28517411e7c
Jon Selzer <jselzer626@gmail.com>
2020-08-19 17:37:35 -0400

    misc changes

--- players/views.py
+++ players/views.py
@@ -4,8 +4,12 @@ from .models import Player, Roster
 
 # Create your views here.
 def loadInitial(request):
-    fullPlayerSet = [{'id': player.playerId, 'displayName': player.displayName, 'team': player.team, 'position': player.position}
-    for player in Player.objects.all()]
+    fullPlayerSet = [
+        {'id': player.playerId, 'displayName': player.displayName, 'team': player.team, 'position': player.position,
+        'profileImg': player.profileImg}
+        for player in Player.objects.all()]
+    
     response = JsonResponse(fullPlayerSet, safe=False)
+    #CORS
     response["Access-Control-Allow-Origin"] = '*'
     return response
